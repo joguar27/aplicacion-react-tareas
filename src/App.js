ee6b0388410e7f51414de37aaaeab694c1b3370d
jorge <jorgegusarruda@gmail.com>
2026-02-02 00:11:36 -0300

    tarea-completada

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import freeCodeCampLogo from "./imagenes/freeCodeCamp-logo.png";
+import Tarea from './componentes/Tarea';
 
 function App() {
   return (
     <div className="aplicacion-tareas">
       <div className="freecodecamp-logo-contenedor">
-        <img src={freeCodeCampLogo} className="freecodecamp-logo" />
+        <img src={freeCodeCampLogo} className="freecodecamp-logo" alt='Logo de FreeCodeCamp' />
       </div>
       <div className="tareas-lista-principal">
         <h1>Mis Tareas</h1>
+        <Tarea texto='Aprender React' />
       </div>
     </div>
   );
